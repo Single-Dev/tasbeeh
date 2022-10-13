b12a58e336aed9c4747ef05f0c37ec8e040ce422
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-10-13 19:07:44 +0500

    bugs fixed

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ let countPlus = document.querySelector('.btn')
 let countZero = document.querySelector('.btn1')
 let input = document.querySelector('.input')
 countZero.style.backgroundColor = "green"
-input.value =  localStorage.clickcount;
 
 countPlus.addEventListener('click' , function(){
-        input.value =  localStorage.clickcount;
-        localStorage.clickcount = Number(localStorage.clickcount)+1;
-        getItem( key ).setItem( key, value ).localStorage.clickcount
-    })
-    
-countZero.addEventListener('click' , function(){
-    localStorage.clickcount = Number(localStorage.clickcount)*0; 
-    input.value =  localStorage.clickcount;
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+    // getItem( key ).setItem( key, value ).localStorage.clickcount
 })
 
-// function clickCounter() {
-//     localStorage.clickcount = Number(localStorage.clickcount)+1;
-// }
+countZero.addEventListener('click' , function(){
+    localStorage.clickcount = Number(localStorage.clickcount)*0; 
+})
+
+setTimeout(() => {
+    localStorage.setItem(localStorage.clickcount=0)
+}, 1);
+
+setInterval(() => {
+    input.value =  localStorage.clickcount;
+},1);
 
 
