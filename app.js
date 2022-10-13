79c0c9ea7915a3398ccf65c716bdd615b8db7e48
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-10-13 17:24:50 +0500

    modifed

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ let countPlus = document.querySelector('.btn')
 let countZero = document.querySelector('.btn1')
 let input = document.querySelector('.input')
 countZero.style.backgroundColor = "green"
-countPlus.addEventListener('click' , function(){
-        input.value = parseInt(input.value) +1;
-        countZero.style.display  = "block"
-        return clickCounter()
-})
-
-countZero.addEventListener('click' , function(){
-    input.value = 0
-        localStorage.clickcount = Number(localStorage.clickcount)*0; 
-})
-
-function clickCounter() {
-    localStorage.clickcount = Number(localStorage.clickcount)+1;
-}
-
 input.value =  localStorage.clickcount;
+
+countPlus.addEventListener('click' , function(){
+        input.value =  localStorage.clickcount;
+        if (typeof(Storage) !== "undefined") {
+            if (localStorage.clickcount) {
+                localStorage.clickcount = Number(localStorage.clickcount)+1;
+            } else {
+              localStorage.clickcount = 0;
+            }
+          } else {
+           alert("Sorry, your browser does not support web storage...")
+          }
+    })
+    
+countZero.addEventListener('click' , function(){
+    localStorage.clickcount = Number(localStorage.clickcount)*0; 
+    input.value =  localStorage.clickcount;
+})
+
+// function clickCounter() {
+//     localStorage.clickcount = Number(localStorage.clickcount)+1;
+// }
+
+
